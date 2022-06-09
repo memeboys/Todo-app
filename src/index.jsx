110d2612f90b5fcc6c5a1e655fb84f14d59295d0
memeboys <simonsnkn@gmail.com>
2022-06-09 20:19:07 +0300

    functional components

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import TodoApp from './class-components/todo-app/todo-app';
+import TodoApp from './functional-components/todo-app/todo-app';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
